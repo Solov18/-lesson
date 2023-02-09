7592f9505166b0d772ef9d302dc95ac211e11fe4
Solov18 <solov18@list.ru>
2023-02-10 01:05:51 +0300

    Cnhfyyst ltkf

--- Examples/Example003/Program.cs
+++ Examples/Example003/Program.cs
@@ -1,3 +1,5 @@
-﻿int numberA = 6;
-int numberb = 5;
+﻿int numberA = new Random().Next(1, 10);
+Console.WriteLine (numberA);
+int numberb = new Random().Next(1, 10);
+Console.WriteLine (numberb);
 Console.WriteLine (numberA + numberb);
